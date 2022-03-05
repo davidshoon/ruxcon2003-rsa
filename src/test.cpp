01d973b103856e136404942eb3d79e7aadb0e699
David Shoon <18284887+davidshoon@users.noreply.github.com>
2022-03-05 15:40:31 +1100

    initial commit

---
+++ src/test.cpp
@@ -0,0 +1,92 @@
+/*
+	Ruxcon 2003 "Attacking RSA Key generators" by icebsd - revisited
+
+	Factorising the 779-bit key using pollard p-1.
+
+	To compile: g++ test.cpp -lgmpxx -lgmp 
+
+	You will need to "sudo apt install libgmp-dev" (ubuntu).
+
+	Author: David Shoon
+
+	Notes: I guess it's almost 20 years onwards since the actual presentation,
+	and I had to pull my own presentation slides from archive.org. LOL. I've since
+	lost all source code which I used for the presentation, including a weak
+	primes key generator, which was used to generate this 779-bit key.
+
+	This is for those who wanted to find out the prime numbers to the various
+	keys in the presentation, but don't know how to code in C/C++.
+
+	I hope it's of some use / knowledge sharing.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <vector>
+#include <iostream>
+
+#include <math.h>
+#include <gmp.h>
+#include <gmpxx.h>
+
+mpz_class pollard_p_minus_1(mpz_class c, mpz_class max, mpz_class n)
+{
+	mpz_class m = c;
+	mpz_class g;
+
+	for (mpz_class i = 1; i < max; i++) {
+		mpz_class res;
+
+		mpz_pow_ui(res.get_mpz_t(), m.get_mpz_t(), mpz_get_ui(i.get_mpz_t()) );
+
+		m = res % n;
+
+		if (i % 10 == 0) {
+			mpz_class temp = m - 1;
+			mpz_gcd(g.get_mpz_t(), temp.get_mpz_t(), n.get_mpz_t());
+			if (g > 1) {
+				if ((n % g == 0) && (g != 1) && (g != n)) {
+					std::cout << "g: " << g << std::endl;
+					return g;
+				}
+			}
+		}
+	}
+
+	return -1;
+}
+
+void func()
+{
+	mpz_class a;
+
+
+	a = "2967712933176139505325218827281016830172885377668890665422303521091925853065808657763875660509418806842054224785177504767070016206910781672103368981812004063609160919203696870514423547321725245287923332418660222579507200000000000000001";
+
+	std::cout << "a: " << a << std::endl;
+
+	mpz_class g = pollard_p_minus_1(2, 1000, a);
+
+	if (mpz_probab_prime_p(g.get_mpz_t(), 50)) {
+		std::cout << "found prime!" << std::endl;
+	}
+
+	std::cout << "prime: " << g << std::endl;
+
+	mpz_class h = a / g;
+
+	if (mpz_probab_prime_p(h.get_mpz_t(), 50)) {
+		std::cout << "found prime!" << std::endl;
+	}
+
+	std::cout << "other prime: " << h << std::endl;
+}
+
+int main()
+{
+	func();
+
+	return 0;
+}
